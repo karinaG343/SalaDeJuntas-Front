408f8eeb4f15e47828718aadf5efc59aef6fae36
Karina Guerrero <karinaguerrero157@gmail.com>
2023-04-26 10:58:24 -0500

    Avance con servicios y rutas de las pagina

--- src/app/reservaciones.service.ts
+++ src/app/reservaciones.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservacionesService {
+  private apiUrl = 'https://localhost:ip/api/reservaciones'; 
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any[]> { 
+    return this.http.get<any[]>(this.apiUrl); 
+  } 
+
+  create(item: any): Observable<any> { 
+    return this.http.post<any>(this.apiUrl, item); 
+  } 
+
+  update(id: number, item: any): Observable<any> { 
+    return this.http.put<any>(`${this.apiUrl}/${id}`, item); 
+  } 
+
+  delete(id: number): Observable<any> { 
+    return this.http.delete<any>(`${this.apiUrl}/${id}`); 
+  }
 }
--- src/app/salas/salas.component.ts
+++ src/app/salas/salas.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { SalasService } from '../salas.service';
 
 @Component({
   selector: 'app-salas',
   templateUrl: './salas.component.html',
   styleUrls: ['./salas.component.css']
 })
-export class SalasComponent {
 
+export class SalasComponent {
+  constructor(private salasService: SalasService) { 
+    
+  }
 }
