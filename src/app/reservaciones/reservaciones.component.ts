c2e3c03fb7d772dfe393c9087cc36a831eeceb97
Karina Guerrero <karinaguerrero157@gmail.com>
2023-04-27 22:07:29 -0500

    Modulo reservaciones: servicio, metodos y bindeos HTML

--- src/app/reservaciones/reservaciones.component.ts
+++ src/app/reservaciones/reservaciones.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SalasService } from '../salas.service';
-import { Observable, Subscriber } from 'rxjs';
-import { JsonPipe } from '@angular/common';
-import { NgbTimepickerModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { ReservacionesService } from '../reservaciones.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-reservaciones',
@@ -13,20 +12,60 @@ import { FormsModule } from '@angular/forms';
 
 export class ReservacionesComponent implements OnInit {
   tablaSala : any;
-
-  constructor(private salasService: SalasService) { 
-    
+  formReservaciones : FormGroup;
+  tablaReservaciones: any;
+  constructor(private salasService: SalasService, private fb : FormBuilder, private toastr : ToastrService, private reservacionService: ReservacionesService) { 
+    this.formReservaciones = this.fb.group({
+      sala:['', Validators.required],
+      fecha_inicio:['', Validators.required],
+      fecha_fin:['', Validators.required]
+    });
   }
 
   ngOnInit(){
     this.GetSala();
-    console.log(this.tablaSala);
+    this.GetReservaciones();
   }
 
   GetSala() : void{
     this.salasService.getAll().subscribe(data => {
       console.log(data);
       this.tablaSala = data;
+    });
+  }
+
+  SetReservaciones() : void{
+    const reservacion = {Id_sala:this.formReservaciones.value.sala, fecha_hora_inicial:this.formReservaciones.value.fecha_inicio, 
+      fecha_hora_final:this.formReservaciones.value.fecha_fin};
+    console.log(reservacion);
+    this.reservacionService.create(reservacion).subscribe(data => {
+      console.log(data);
+      this.toastr.success('Reservación con exito', 'Titulo');
+      this.GetReservaciones();
+    }, error => {
+      console.log(error);
+      this.toastr.error(error.error.detail,'Titulo');
+    });
+  }
+
+  GetReservaciones() : void{
+    this.reservacionService.getAll().subscribe(data => {
+      console.log(data);
+      this.tablaReservaciones = data;
+    });
+  }
+
+  LiberarReservacion(reservacion:any): void{
+    const salaReservacion = {id: reservacion.id, id_sala: reservacion.id_sala, fecha_hora_inicial: reservacion.fecha_hora_inicial, 
+      fecha_hora_final: reservacion.fecha_hora_final, liberado: true};
+      console.log(salaReservacion);
+    this.reservacionService.update(reservacion.id, salaReservacion).subscribe(data => {
+    this.GetReservaciones();
+    this.toastr.success('Liberado con exito', 'Titulo');
+    console.log(data);
+    }, error => {
+    this.toastr.error(error.error.detail, 'Titulo');
+    console.log(error);
     });
   }
 }
--- src/app/salas/salas.component.ts
+++ src/app/salas/salas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SalasService } from '../salas.service';
 import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
@@ -10,14 +10,22 @@ import { Observable, Subscriber } from 'rxjs';
   styleUrls: ['./salas.component.css']
 })
 
-export class SalasComponent {
+export class SalasComponent implements OnInit {
   salaForm : FormGroup;
   tablaSala : any;
+  sala : any;
+  id : number | undefined;
+  accion = 'Guardar';
 
   constructor(private salasService: SalasService, private fb : FormBuilder, private toastr : ToastrService) { 
     this.salaForm = this.fb.group({
       sala:['', Validators.required]
     })
+  }
+
+  ngOnInit(){
+    this.GetSala();
+    console.log(this.tablaSala);
   }
 
   GetSala() : void{
@@ -36,15 +44,37 @@ export class SalasComponent {
 
   SetSala() : void{
     const nombre_sala = this.salaForm.value.nombre;
-    const salaObj = {nombre_sala : this.salaForm.value.sala}
-    this.salasService.create(salaObj).subscribe(data => {
-      this.toastr.success("Sala creada", "Titulo");
-      this.salaForm.reset();
-      this.GetSala();
-    }, error => {
-      this.toastr.error("Error sala no creada", "Titulo");
+    const salaObj = {nombre_sala : this.salaForm.value.sala};
+    const salaObjPut = {id : this.id, nombre_sala : this.salaForm.value.sala};
+    if(this.id == undefined){
+      this.salasService.create(salaObj).subscribe(data => {
+        this.toastr.success("Sala creada", "Titulo");
+        this.salaForm.reset();
+        this.GetSala();
+      }, error => {
+        this.toastr.error("Error sala no creada", "Titulo");
+      }
+      );
+      console.log(this.salaForm);
+    }else{
+      this.salasService.update(this.id, salaObjPut).subscribe(data => {
+        this.toastr.success("Sala actualizada", "Titulo");
+        this.salaForm.reset();
+        this.GetSala();
+        this.id = undefined;
+        this.accion = "Guardar";
+      }, error => {
+        this.toastr.error("Error no se actualizo la sala");
+        console.log(this.id, salaObjPut, error);
+      });
     }
-    );
-    console.log(this.salaForm);
   }
+
+  PutSala(sala:any) : void{
+    console.log(sala);
+    this.salaForm.patchValue({sala:sala.nombre_sala});
+    this.id = sala.id;
+    this.accion = "Actualizar";
+  }
+
 }
